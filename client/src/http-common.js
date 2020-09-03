b90afa0a322f0017c62d8d8088c993defc28eb73
dys907 <dys907@gmail.com>
2020-09-03 01:14:22 -0700

    env variable changes

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-let baseUrl;
-if(process.env.NODE_ENV === "development") {
-    baseUrl =  "http://localhost:5001/api"
-} else {
-    baseUrl = "https://misconnect-server.herokuapp.com/api"
-}
+// let baseUrl;
+// if(process.env.NODE_ENV === "development") {
+//     baseUrl =  "http://localhost:5001/api"
+// } else {
+//     baseUrl = "https://misconnect-server.herokuapp.com/api"
+// }
 
 //const baseUrl = process.env.REACT_APP_BASEURL || "http://localhost:5001/api"
 
 export default axios.create({
-    baseURL: baseUrl,
+    baseURL: process.env.REACT_APP_BASEURL,
     headers: {
         "Content-type": "application/json"
     }
