3434363d6fff0c4c6bd461f7181a39520107d660
dys907 <dys907@gmail.com>
2020-09-02 22:45:31 -0700

    baseurl change

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseUrl = process.env.BASEURL || "http://localhost:5001/api"
 
 export default axios.create({
-    baseURL: baseUrl,
+    baseURL: process.env.BASEURL || "http://localhost:5001/api",
     headers: {
         "Content-type": "application/json"
     }
