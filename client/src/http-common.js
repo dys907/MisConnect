32e4e43327849432e81c37940b3d8d4ffd5a901e
dys907 <dys907@gmail.com>
2020-09-03 00:49:15 -0700

    logic to env

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-const baseUrl = process.env.BASEURL || "http://localhost:5001/api"
+let baseUrl;
+if(process.env.NODE_ENV === "development") {
+    baseUrl =  "http://localhost:5001/api"
+} else {
+    baseUrl = "https://misconnect-server.herokuapp.com/api"
+}
+
+//const baseUrl = process.env.REACT_APP_BASEURL || "http://localhost:5001/api"
 
 export default axios.create({
     baseURL: baseUrl,
