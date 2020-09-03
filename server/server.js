eb6042ca759713904214e6afd11b5c3f73a5c5f1
dys907 <dys907@gmail.com>
2020-09-02 21:16:03 -0700

    port changes

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //connect mongodb
-const db = require("../server/models");
+const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 
 require("../server/routes/profsearch.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
